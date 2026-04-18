be76c4b06de2bbfe341366be0b9e610ad96b7245
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-18 08:17:54 +0530

    Creating Table And Inserting Data

--- src/main/java/com/dilusha/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/dilusha/springdatajpa/SpringDataJpaApplication.java
@@ -1,13 +1,37 @@
 package com.dilusha.springdatajpa;
 
+import com.dilusha.springdatajpa.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(SpringDataJpaApplication.class, args);
+
+        StudentRepo repo = context.getBean(StudentRepo.class);
+
+        Student s1 = context.getBean(Student.class);
+        Student s2 = context.getBean(Student.class);
+        Student s3 = context.getBean(Student.class);
+
+        s1.setRollNo(100);
+        s1.setName("Dilusha");
+        s1.setMark(92);
+
+        s2.setRollNo(101);
+        s2.setName("Kamal");
+        s2.setMark(88);
+
+        s3.setRollNo(102);
+        s3.setName("Hashan");
+        s3.setMark(79);
+
+        repo.save(s1);
+        repo.save(s2);
+        repo.save(s3);
+    }
 
 }
--- src/main/java/com/dilusha/springdatajpa/StudentRepo.java
+++ src/main/java/com/dilusha/springdatajpa/StudentRepo.java
@@ -1,4 +1,9 @@
 package com.dilusha.springdatajpa;
 
-public class StudentRepo {
+import com.dilusha.springdatajpa.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Integer> {
 }
