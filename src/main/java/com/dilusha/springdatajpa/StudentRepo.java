63b5eecf12274d2b84ed8cb7d9836239a4b12b88
Dilusha <chakrawarthigedilushaeshan@gmail.com>
2026-04-18 08:43:12 +0530

    JPQL Query

--- src/main/java/com/dilusha/springdatajpa/StudentRepo.java
+++ src/main/java/com/dilusha/springdatajpa/StudentRepo.java
@@ -2,8 +2,18 @@ package com.dilusha.springdatajpa;
 
 import com.dilusha.springdatajpa.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+
+    List<Student> findByName(String name);
+
+    List<Student> findByMark(int mark);
+
+    List<Student> findByMarkGreaterThan(int mark);
 }
